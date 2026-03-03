d6d504eadeea60609c22ceb1399fb5f1ed8b9392
tiagokloehnn <tiagokloehnn@gmail.com>
2026-03-03 20:34:59 -0300

    voltando ao padrão

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ function Home() {
     return (
         <div>
             <h1>Seja bem vindo ao site</h1>
-            <p>Você é muito importante pra mim, te amo muito!</p>
+            <p>Testando site</p>
         </div>
     );
 
