b2b69873610fb4115b2d14e4fd6b345e241258a3
Tiago Kloehn <68929714+tiagokloehnn@users.noreply.github.com>
2026-03-03 00:22:28 +0000

    alterando as definições de porta como pública

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+function Home() {
+    return (
+        <div>
+            <h1>Seja bem vindo ao site</h1>
+            <p>Essa aqui é a página principal do projeto.</p>
+        </div>
+    );
+
+}
+
+export default Home;
